2427f67d7b1e4961ab3238c009f5b992e4be12e6
ChutLuis <l.chuta2014ig@gmail.com>
2017-11-16 19:41:49 -0600

    Adding Text Reader

---
+++ src/gt/edu/url/proyecto2/dataStructures/Position.java
@@ -0,0 +1,21 @@
+package gt.edu.url.proyecto2.dataStructures;
+
+// TODO: Auto-generated Javadoc
+/**
+ * The Interface Position.
+ *
+ * @param <E>
+ *            the element type
+ */
+public interface Position<E> {
+	
+	/**
+	 * Gets the element.
+	 *
+	 * @return the element
+	 * @throws IllegalStateException
+	 *             the illegal state exception
+	 */
+	E getElement() throws IllegalStateException;
+
+}
--- src/gt/edu/url/proyecto2/diseño/Principal.java
+++ src/gt/edu/url/proyecto2/diseño/Principal.java
@@ -1,11 +1,28 @@
 package gt.edu.url.proyecto2.diseño;
-
+import gt.edu.url.proyecto2.dataStructures.DoubleLinkedList;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
+import java.awt.TextArea;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import javax.swing.JFileChooser;
+
+import java.awt.event.ActionListener;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Scanner;
+import java.util.stream.Stream;
+import java.awt.event.ActionEvent;
+import javax.swing.JTextArea;
+import javax.swing.DropMode;
 
 public class Principal extends JFrame {
 
@@ -35,9 +52,55 @@ public class Principal extends JFrame {
 		setBounds(100, 100, 498, 420);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
-		contentPane.setLayout(new BorderLayout(0, 0));
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		JTextArea textArea = new JTextArea();
+		textArea.setEditable(false);
+		textArea.setBounds(164, 37, 269, 283);
+		contentPane.add(textArea);
+		
+		JButton btnSelectFile = new JButton("Select File");
+		btnSelectFile.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {			
+				DoubleLinkedList<Integer> bTree = new DoubleLinkedList<>();			
+							
+				JFileChooser chooser = new JFileChooser();
+				chooser.showOpenDialog(null);
+				File f = chooser.getSelectedFile();
+				String filename = f.getAbsolutePath();
+				
+				try {
+					Scanner n1= new Scanner(f);
+					FileReader reader = new FileReader(filename);
+					BufferedReader br= new BufferedReader(reader);
+					textArea.read(br, null);
+					br.close();
+					textArea.requestFocus();
+					if (n1.findInLine("btree") != null) {						
+						n1.nextLine();
+						do {						 
+						 int a = n1.nextInt();
+						 bTree.addFirst(a);						 
+						}while(n1.hasNext());
+					}
+					
+					
+					
+				}catch(Exception e)
+				{
+					
+				}
+				int b=0;
+				do {
+					b= bTree.removeLast();
+					System.out.println(b);
+				}while(b!=0);
+			}
+		});
+		btnSelectFile.setBounds(32, 38, 89, 23);
+		contentPane.add(btnSelectFile);
+		
+		
 	}
-
 }
 //no se que esta pasando :'v
